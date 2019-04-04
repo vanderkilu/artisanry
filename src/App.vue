f13938296e2c29f99d08393709ad85ddd1e9defa
vanderkilu <vanderkiliku@gmail.com>
2019-04-04 17:35:25 -0400

    created slide1

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <router-view></router-view>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<style scoped>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+@media (min-width: 320px) {
+  html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+  }
+  *, *::before, *::after {
+    font-family:  'Montserrat', sans-serif;
+  }
 }
 </style>
---
+++ src/components/Slide.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="slide">
+        <img src="../assets/slide1.svg" alt="colloborate slide 1" class="slide__svg">
+        <h3 class="slide__text--large">Colloborate</h3>
+        <p class="slide__text--small">Join and Colloborate with friends and new people to generate ideas</p>
+    </div>
+</template>
+
+
+<style scoped>
+    @media (min-width: 320px) {
+        .slide {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            padding: 2rem;
+            margin-top: 6rem;
+        }
+        .slide__svg {
+            width: 100%;
+        }
+        .slide__text--large {
+            font-size: 2rem;
+            font-weight: 600;
+            color: #4a148c;
+            margin: 2rem 0;
+        }
+        .slide__text--small {
+            font-size: 1.4rem;
+            color: #9e9e9e;
+            text-align: center;
+        }
+    }
+</style>
