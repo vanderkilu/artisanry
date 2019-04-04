e9c54d16ee999a7906d15bda8713345d6370997e
vanderkilu <vanderkiliku@gmail.com>
2019-04-04 18:06:24 -0400

    adding a back arrow

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="slide">
-        <img src="../assets/slide1.svg" alt="colloborate slide 1" class="slide__svg">
-        <h3 class="slide__text--large">Colloborate</h3>
-        <p class="slide__text--small">Join and Colloborate with friends and new people to generate ideas</p>
-        <div class="slide__controls">
-            <img class="slide__arr" src="../assets/back.svg">
-            <img class="slide__arr" src="../assets/next.svg">
+    <div class="wrapper">
+        <div class="slide">
+            <img src="../assets/slide1.svg" alt="colloborate slide 1" class="slide__svg">
+            <h3 class="slide__text--large">Colloborate</h3>
+            <p class="slide__text--small">Join and Colloborate with friends and new people to generate ideas</p>
+            <div class="slide__controls">
+                <img class="slide__arr back" src="../assets/back.svg">
+                <img class="slide__arr next" src="../assets/next.svg">
+            </div>
         </div>
     </div>
 </template>
@@ -35,10 +37,16 @@
             color: #9e9e9e;
             text-align: center;
         }
+        .slide__controls {
+            flex-basis: 100%;
+        }
         .slide__arr {
             margin-top: 2rem;
-            width: 3rem;
-            height: 3rem;
+            width: 2.5rem;
+            height: 2.5rem;
+        }
+        .back {
+            margin-right: 5rem;
         }
     }
 </style>
