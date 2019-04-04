1abb41fe460888679b5227d54794b3940af4ad40
vanderkilu <vanderkiliku@gmail.com>
2019-04-04 18:32:41 -0400

    adding control effect tocontrol buttons

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -1,16 +1,56 @@
 <template>
     <div class="wrapper">
-        <div class="slide">
+        <div class="slide" v-if="incrementor === 1">
             <img src="../assets/slide1.svg" alt="colloborate slide 1" class="slide__svg">
             <h3 class="slide__text--large">Colloborate</h3>
             <p class="slide__text--small">Join and Colloborate with friends and new people to generate ideas</p>
             <div class="slide__controls">
-                <img class="slide__arr back" src="../assets/back.svg">
-                <img class="slide__arr next" src="../assets/next.svg">
+                <img class="slide__arr back" src="../assets/back.svg" @click="back">
+                <img class="slide__arr next" src="../assets/next.svg" @click="next">
+            </div>
+        </div>
+        <div class="slide" v-if="incrementor === 2">
+            <img src="../assets/slide1.svg" alt="colloborate slide 1" class="slide__svg">
+            <h3 class="slide__text--large">Colloborate</h3>
+            <p class="slide__text--small">Join and Colloborate with friends and new people to generate ideas</p>
+            <div class="slide__controls">
+                <img class="slide__arr back" src="../assets/back.svg" @click="back">
+                <img class="slide__arr next" src="../assets/next.svg" @click="next">
+            </div>
+        </div>
+        <div class="slide" v-if="incrementor === 3">
+            <img src="../assets/slide1.svg" alt="colloborate slide 1" class="slide__svg">
+            <h3 class="slide__text--large">Colloborate</h3>
+            <p class="slide__text--small">Join and Colloborate with friends and new people to generate ideas</p>
+            <div class="slide__controls">
+                <img class="slide__arr back" src="../assets/back.svg" @click="back">
+                <img class="slide__arr next" src="../assets/next.svg" @click="next">
             </div>
         </div>
     </div>
 </template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            incrementor: 1,
+            howFar: 3
+        }
+    },
+    methods: {
+        next() {
+            if (this.incrememtor >= this.howFar) return;
+            return this.incrementor++
+        },
+        back() {
+            if (this.incrementor < 1) return;
+            return this.incrementor--
+        }
+    }
+}
+</script>
 
 
 <style scoped>
