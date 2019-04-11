2c55be21ba36ad4677ace7e037303847147c4b64
vanderkilu <vanderkiliku@gmail.com>
2019-04-11 11:24:48 -0400

    setting up register controller

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="auth">
         <p class="auth__text--main">Connect now</p>
-        <form  class="auth__form">
-            <input type="text" class="input" placeholder="username">
-            <input type="email" class="input" placeholder="email">
-            <input type="text" class="input" placeholder="number">
-            <button class="btn btn-fill" @click.prevent="routeTo">Sign up</button>
+        <form  @submit.prevent="signup" class="auth__form">
+            <input type="text" class="input" placeholder="username" v-model="name">
+            <input type="email" class="input" placeholder="email" v-model="email">
+            <input type="text" class="input" placeholder="number" v-model="number">
+            <input type="text" class="input" placeholder="password" v-model="password">
+            <button class="btn btn-fill">Sign up</button>
             <p class="auth__text--small">Already have an account?    
               <br><router-link :to="{name: 'login'}">sign in here</router-link></p>
         </form>
@@ -14,12 +15,28 @@
 
 <script>
 import { EventBus } from '../main'
+import { register,storeToken } from '../services'
 export default {
     data() {
         return {
-            routeTo() {
-                this.$router.push({name: 'artisans'})
+           name: '',
+           email: '',
+           number: '',
+           password: ''
+        }
+    },
+    methods: {
+        async signup() {
+            const data = {
+                name: this.name,
+                email: this.email,
+                phone: this.number,
+                password: this.password
             }
+            let response = await register(data)
+            console.log(response)
+            storeToken(response.data._id)
+            this.$router.push({name: 'artisans'})          
         }
     },
     mounted() {
